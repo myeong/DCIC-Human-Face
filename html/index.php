8047a939913d6e97d18322cc016b15f574a500b2
Myeong Lee <deeper.myeong.lee@gmail.com>
2017-01-19 16:28:38 -0500

    baseline polygons

--- html/index.php
+++ html/index.php
@@ -2,19 +2,33 @@
 <head>
 <title>Human Face</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" type="text/css" href="style.css" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type='text/javascript' src='http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js'></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
-<link rel="stylesheet" href="//apps.bdimg.com/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
-<script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+<!-- <script src="//apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script> -->
+<script src="//apps.bdimg.com/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<script src="js/leaflet.ajax.min.js"> </script>
+<script src="source/with_date.js"> </script>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" href="//apps.bdimg.com/libs/jqueryui/1.10.4/css/jquery-ui.min.css">
+
+<!-- <script type='text/javascript' src='http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js'></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" /> -->
+<script>
+	function getYear(){
+			var input = document.getElementById('fname').value;
+			document.getElementById('demo').value=input;
+			// Get slider value
+			return input;
+	}
+</script>
 </head>
 <body>
 	<div id="site">
 		<div id="header">																										
-			<div id="menu">
+			<!-- <div id="menu">
 				<ul>
 					<li><a href="#" class="but1"><img src="images/spacer.gif" alt="" width="106" height="42" /></a></li>
 					<li><a href="#" class="but2"><img src="images/spacer.gif" alt="" width="118" height="42" /></a></li>
@@ -22,7 +36,7 @@
 					<li><a href="#" class="but4"><img src="images/spacer.gif" alt="" width="99" height="42" /></a></li>
 					<li><a href="#" class="but6"><img src="images/spacer.gif" alt="" width="129" height="42" /></a></li>
 				</ul>
-			</div>
+			</div> -->
 		</div>
 		
 		
@@ -33,151 +47,85 @@
 				</div>
 				
 <script>
-			var map = L.map('map').setView([35.5861, -82.5554], 16);
-			
-			// load a main layer
-			var baseMap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-							{				
-								center:[35.5861, -82.5554],
-							    //Zoom level
-								maxZoom: 19,
-								minZoom: 14
-							}).addTo(map);
-				
-        	
-			// load polygons
+$(document).ready(function(){
+	var map = L.map('map').setView([35.5861, -82.5554], 16);
 
-			var geojsonURL ='with_date.geojson';		
-			// Turn the color to green when click the polygon
-			var fillColor = {
-				"fillColor": "#0000FF",
-				"opacity": 1
-			};			
-		    // Basci color of the polygon layer
-			var basicColor = {
-			    "color":"#0000FF",
-				"fillColor":"#0000FF",
-				// "opacity": 1,
-			};				
-
-			var clusters = L.layerGroup();
-			
-			var promise= $.getJSON(geojsonURL,function(data) {
-				
-
-				var selected= L.geoJson(data, {
-					style: function(feature) {
-						//console.log(feature.properties);
-						if (feature.properties.offer_made < 1970) {
-				            return basicColor;
-				        } else {
-				        	return {color: "yellow"};
-				        }
-						//return  basicColor;
-					}
-				});
+	var baseMapIndex = {
+	  "Map": baseMap
+	};
 	
+	// load a main layer
+	var baseMap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+					{				
+						center:[35.5861, -82.5554],
+					    //Zoom level
+						maxZoom: 19,
+						minZoom: 14,
+						layers: baseMap
+					}).addTo(map);
 
-				// Add to map
-				selected.on('click', function (e) {
+	var hoverColor = {
+	    fillColor:"yellow"
+	};	
 
-				// Check for selected
-					
-					if (selected) {
-						// Reset selected to default style
-						e.target.setStyle(basicColor);
-					};
+	var defaultStyle = {
+		fillColor: "#0000FF",
+		color: "white",
+		weight: 1,
+		fillOpacity: 1
+	};	
 
-					// Assign new selected
-					selected = e.layer;
-					// Bring selected to front
-					selected.bringToFront();	
-			
-					selected.bindPopup(selected.feature.properties.st_num  + ' ' +selected.feature.properties.st_name);
-					selected.setStyle(fillColor);
-			
-				});
+	var before1970 = {
+		fillColor: "#FF0000",
+		color: "white",
+		weight: 1,
+		fillOpacity: 1
+	};	
+	
+	// When a polygon is clicked
+	function onEachFeature(feature, layer) {
+			var popupContent = "<p>This parel's ID is " +
+					feature.properties.id + ", and Block number is " + feature.properties.block +"</p>";
 
-			    selected.addTo(map);
-				
-				
-				//Legend
-				var legend = L.control({position: 'bottomleft'});
+			if (feature.properties && feature.properties.popupContent) {
+				popupContent += feature.properties.popupContent;
+			}
 
-				legend.onAdd = function (map) {
+			layer.on("mouseover", function (e) {		        
+		        layer.setStyle(hoverColor);
+	    	});
 
-					var div = L.DomUtil.create('div', 'info legend'),
-					grades = [" "],
-					labels = ["1.png"];
-
-					// loop through our density intervals and generate a label with a colored square for each interval
-					for (var i = 0; i < grades.length; i++) {
-						div.innerHTML +=
-						grades[i] + (" <img src="+ labels[i] +" height='200' width='200' style='opacity:0.8'>") +'<br>';
-					}
-
-					return div;
-				};
-
-				legend.addTo(map);
-					// Bring popup on the top	
-				$(".leaflet-map-pane svg").css("z-index", 0);
+	    	layer.on("mouseout", function (e) {
+	    		if (feature.properties.offer_made < getYear()) {
+		            layer.setStyle(before1970);
+		        } else {
+		        	layer.setStyle(defaultStyle);
+		        }
+		        
 			});
-			clusters.addLayer(promise);
-			
-			// console.log(clusters.hasLayer(promise));
-			layerControl = L.control.layers().addTo(map);
-			layerControl.addOverlay(clusters , "Parcels");			
 
-			
-			function myFunction(){  
-				var input = document.getElementById('fname').value;
-				document.getElementById('demo').value=input;
-				// Get slider value
-				var count=input;
-
-				
-				// map.eachLayer(function (layer) {
-				// 	console.log(layer);
-				//     // map.removeLayer(layer);
-				// });
-			
-				final_title=[];
-				offer_made=[];
-				offer_accepted=[];
-				rejected=[];
-				removed=[];
-				
-				var hu=promise.responseJSON.features;
-			
-				var length=promise.responseJSON.features.length;
-				var colors;
-				
-				for (var i = 0; i <length; i++) {
-					//Store different year data
-					final_title[i]=hu[i].properties.final_title;
-					offer_made[i]=hu[i].properties.offer_made;
-					offer_accepted[i]=hu[i].properties.offer_accepted;
-					rejected[i]=hu[i].properties.rejected;
-					removed[i]=hu[i].properties.removed;
+			layer.bindPopup(popupContent);
+	}
 	
-					// Find the color 
-					if (count>=offer_made[i] &&  count< offer_accepted[i]) {colors='yellow'} 
-					else if (count>=offer_accepted[i]  && count< rejected[i] ) {colors='green'}
-					else if (count>=rejected[i] && count< final_title[i]) {colors='red'}
-					else if (count>=final_title[i] && count<removed[i]) {colors='grey'}
-					else if (count>removed[i]) {colors='black'}
-					else {colors='blue'};
-					
-					var cc={ "fillColor":"colors"};
-					
-					//Set the  Color ?????
-					
-					
-		
-				};		
-						
-			};
+
+	// drawing a Polygon Layer
+	L.geoJSON([parcels], {
+
+		style: function(feature) {
+	        if (feature.properties.offer_made < getYear()) {
+	            return before1970;
+	        } else {
+	        	return defaultStyle;	
+	        }
+	    },
+
+		onEachFeature: onEachFeature,
+
+
+	}).addTo(map);
+
+	
+});
 				
 </script>
 	
@@ -191,9 +139,9 @@
 							<input type="text" id="demo" style="background-color:transparent;border:0;  font-size:18px;color:#f6931f; font-weight:bold;">
 							</li>
 						</ul>
-						<input type="range" id="fname" min="1960" max="1976" step="1" value="1960" onchange="myFunction()">
+						<input type="range" id="fname" name="fname" min="1960" max="1976" step="1" value="1960" onchange="getYear()">
 	                    <script>
-							document.getElementById("demo").value=1960;
+							document.getElementById("demo").value=1960;							
 						</script>	
 					
 					</div>
