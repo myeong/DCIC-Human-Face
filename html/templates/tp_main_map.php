36f5932ada1711c8743fe8ae23a9bb1eae6c276e
Myeong Lee <deeper.myeong.lee@gmail.com>
2017-04-25 13:44:49 -0400

    parcel coloring

--- html/templates/tp_main_map.php
+++ html/templates/tp_main_map.php
@@ -696,12 +696,9 @@ $.when(load_data(), get_image_paths()).done(function() {
 				ecmax=20000;
 			};
 
-			if (SLIDER_VALUE == 1960){
+			if (SLIDER_VALUE >= 1960){
 				layer.setStyle(c1);
 			} else {
-				if (SLIDER_VALUE == 1960){
-					layer.setStyle(c1);
-				} 
 				
 				if (SLIDER_VALUE >=todmax && SLIDER_VALUE <= ommax){
 					layer.setStyle(c2);
