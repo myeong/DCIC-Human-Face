20db01ea14879340790b66123d9cfd993507f414
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-06-21 20:33:01 -0400

    Parcels Info

--- admin_interface/parcels.php
+++ admin_interface/parcels.php
@@ -1,11 +1,18 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
   <?php
-  require 'credentials.php';
-  $connect = pg_connect('host=' . DBHOST . ' dbname=' .DBNAME . ' user=' .DBUSER . ' password=' . DBPASS);
+  require 'credentials.inc.php';
+  ini_set('display_errors', 1);
+  $connect = pg_connect('host=' . DBHOST . ' dbname=' . DBNAME . ' user=' . DBUSER . ' password=' . DBPASS);
   if (!$connect){
-  	echo "Error in connection!";
+  	die("Error in connection!:");
   }
 else{
   echo "DB connection successful";
