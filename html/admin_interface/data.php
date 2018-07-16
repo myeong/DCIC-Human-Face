170eb9f670ed728e4c5c34ca18a691aefdd32492
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-07-16 15:44:30 -0400

    data

--- html/admin_interface/data.php
+++ html/admin_interface/data.php
@@ -9,12 +9,6 @@
 
   <title>DCIC Human Face of Big Data</title>
 
-  <script>
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"
-  </script>
-
 <?php
 //Connection to PostgreSQL
 require '../credentials.inc.php';
@@ -25,14 +19,21 @@ if (!$connect){
 else{
   echo "Successfully connected to database:" . " " .pg_dbname() . " on " . pg_host();
 }
-
-//Insert Data into Parcel table
-//Edit Data in Parcel table
-//Delete Data in Parcel Table
-
 ?>
 
 </head>
 <body>
+  <form>
+  <div class="form-group">
+  <label>Parcel ID</label>
+  <input type="text" class="form-control">
+  </div>
+  </form>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
--- html/admin_interface/parcels.php
+++ html/admin_interface/parcels.php
@@ -44,8 +44,8 @@ else{
 <!-- Bootstrap Table Button Groups-->
 
     <div class="float-md-left" id="toolbar">
-      <form class="form-group" action="data.php" name="data">
-      <button type="button" class="btn btn-danger my-2 my-sm-0" name="delete" type="submit"
+      <form class="form-group" method="post" action="data.php" name="data">
+      <button class="btn btn-danger my-2 my-sm-0" name="delete" type="submit" value="submit"
       data-toggle="popover"
       title="Delete an Existing Record"
       data-trigger="hover"
