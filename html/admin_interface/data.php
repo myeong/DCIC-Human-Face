4b404c38b1204ee0caeacd749c0aa0fde8649de2
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-07-16 22:24:52 -0400

    Data

--- html/admin_interface/data.php
+++ html/admin_interface/data.php
@@ -8,6 +8,12 @@ if (!$connect){
 else{
   echo "Successfully connected to database:" . " " .pg_dbname() . " on " . pg_host();
 }
+//Store post data to array (parcels table)
+$parcel_id = $_POST['parcel_id'];
+$block_no = $_POST['block_no'];
+$parcel_no = $_POST['parcel_no'];
+$ward_no = $_POST['ward_no'];
+$land_use = $_POST['land_use'];
 
 //Inserting Data into Parcels table
 if(isset($_POST['add'])){}
