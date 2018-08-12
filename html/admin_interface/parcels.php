463ee5f58995dd8b0d7f89395bdfd14cbdb63da7
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-08-11 22:54:29 -0400

    Javascript

--- html/admin_interface/parcels.php
+++ html/admin_interface/parcels.php
@@ -114,19 +114,22 @@ else{
         data-trigger="hover"
         data-content="hide/show pagination"
         data-placement="bottom">
-        <i class="fas fa-minus-square"></i></button>
+        <i class="fas fa-minus-square"></i>
+      </button>
         <button type="button" class="btn btn-dark"
         data-toggle="popover"
         data-trigger="hover"
         data-content="Hide table"
         data-placement="bottom">
-        <i class="fas fa-table"></i></button>
+        <i class="fas fa-table"></i>
+      </button>
         <button type="button" class="btn btn-dark"
           data-toggle="popover"
           data-trigger="hover"
           data-content="refresh"
           data-placement="bottom">
-          <i class="fas fa-sync-alt"></i></button>
+          <i class="fas fa-sync-alt"></i>
+        </button>
       </div>
 
   <br><br>
