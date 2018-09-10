eacdd17d863942aeae6b2d3b3ffb5fb8d1b3481e
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-09-09 22:59:45 -0400

    javascript

--- html/admin_interface/parcels.php
+++ html/admin_interface/parcels.php
@@ -193,6 +193,8 @@ else{
       <li class="page-item"><a class="page-link" href="#">1</a></li>
       <li class="page-item"><a class="page-link" href="#">2</a></li>
       <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      <li class="page-item"><a class="page-link" href="#">5</a></li>
       <!-- Arrow Right -->
       <li class="page-item">
         <a class="page-link" href="#" aria-label="Next">
