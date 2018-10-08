b257a1d7967ebf3d478f1e7a30622325886ca63c
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-10-08 19:58:29 -0400

    Javascript

--- html/admin_interface/parcels.php
+++ html/admin_interface/parcels.php
@@ -238,7 +238,7 @@ else{
                  alert("url" + url);
                  alert("line number" + line);
               }
-
+//Table pagination
   function pagination() {
     $("table").each(function(){
       //Variables
@@ -265,20 +265,14 @@ else{
       $pager.insertBefore($pagi).find('span.page-number:first').addClass('active');
     });
   }
+  
+//Table Sort
+  function sort(){
+    var table;
+    table = document.getElementById("table");
+  }
 
-  /*function sort(table_id, sortColumn){
-    var tableData = document.getElementById('table').getElementsByTagName('tbody').item(0);
-      var rowData = tableData.getElementsByTagName('tr');
-      for(var i = 0; i < rowData.length - 1; i++){
-          for(var j = 0; j < rowData.length - (i + 1); j++){
-              if(Number(rowData.item(j).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, "")) < Number(rowData.item(j+1).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, ""))){
-                  tableData.insertBefore(rowData.item(j+1),rowData.item(j));
-              }
-          }
-      }
-}*/
-
-  //Search filter
+//Search filter
   function filter(){
     var input, filter, table, tr, td, x, q;
       input = document.getElementById("input");
