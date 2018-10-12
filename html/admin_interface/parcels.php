1e44d6b8828a20197ecad218c670a79a750e03d9
Shishiri Nganyone <snganyon@terpmail.umd.edu>
2018-10-11 20:02:44 -0400

    admin interface clean push

--- html/admin_interface/parcels.php
+++ html/admin_interface/parcels.php
@@ -4,8 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="stylesheet" href="main_php.css">
-
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
