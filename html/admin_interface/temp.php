284d1f407a7daac33f517f5cac9e60c001820abf
Hyeong-gi <hyeonghong90@gmail.com>
2018-10-24 20:08:53 -0400

    temporary file is removed

--- html/admin_interface/temp.php
+++
@@ -1,5 +0,0 @@
-div class="col-md-4">
-							<label><?php echo trim($key);?><small class="required">*</small></label>
-
-							<input type="text" class="form-control" id=<?php echo 'event_type' . $index; ?> event_type_id="<?php echo $event_info[$i]['event_type_id']; ?>" value="<?php echo trim($value);?>" required minlength="1" onblur="pageUpdate(id)">
-						</div>
